d0fecd088bbb4fa6264756666de995bc7c6e0a6d
Kaan Çolak <kaancolak3458@gmail.com>
2023-05-30 12:02:11 +0300

    updated

---
+++ PetikoyVeterinaryEntityLayer/Models/BaseNonNumeric.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetikoyVeterinaryEntityLayer.Models
+{
+    public class BaseNonNumeric
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key] //Primary Key
+        [Column(Order =1)]
+        public virtual string Id { get; set; }
+
+        [Column(Order =2)]
+        public DateTime CreatedDate { get; set; }
+
+        public bool IsRemoved { get; set; }
+
+    }
+}
