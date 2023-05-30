4dc62086f7c97b472acbb71dcb8f594f531b88ce
Kaan Çolak <kaancolak3458@gmail.com>
2023-05-30 09:13:20 +0300

    Created MVC Player

---
+++ PetikoyVeterinary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PetikoyVeterinary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
